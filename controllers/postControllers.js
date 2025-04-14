0b7cc6ab29bce6b916225d32ed6fafd1beee715a
Willians-Hurtado <willianshurtado5@gmail.com>
2025-04-14 15:44:46 +0200

    milestone 4

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -16,7 +16,19 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    res.json(`show the post with id: ${req.params.id}`)
+    /* res.json(`show the post with id: ${req.params.id}`) */
+
+    const id = req.params.id
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
+        res.json(results[0]);
+
+
+    })
 }
 
 function store(req, res) {
